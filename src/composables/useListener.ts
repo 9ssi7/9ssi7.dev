821895933f562e0445a1b7baa5ea867f9a94fd59
9ssi7 <info@ssibrahimbas.com>
2024-03-21 20:17:20 +0300

    remove the old application

--- src/composables/useListener.ts
+++
@@ -1,9 +0,0 @@
-export default function useListener(name: string, callback: () => void) {
-  onMounted(() => {
-    window.addEventListener(name, callback, false);
-  });
-
-  onUnmounted(() => {
-    window.removeEventListener(name, callback, false);
-  });
-}
